6b2ba332a5e99dfaabc75c9f7aa1b604be73e5bf
Александр Новиков <anovikov@tsum.ru>
2021-08-27 16:36:19 +0300

    WIP

--- graph.go
+++ graph.go
@@ -1,35 +1,35 @@
 package semnet
 
-import (
-	"github.com/mem-memov/semnet/internal"
-	"github.com/mem-memov/semnet/internal/action"
-	"github.com/mem-memov/semnet/internal/bit"
-	"github.com/mem-memov/semnet/internal/character"
-	"github.com/mem-memov/semnet/internal/word"
-)
+//import (
+//	"github.com/mem-memov/semnet/internal"
+//	"github.com/mem-memov/semnet/internal/action"
+//	"github.com/mem-memov/semnet/internal/bit"
+//	"github.com/mem-memov/semnet/internal/character"
+//	"github.com/mem-memov/semnet/internal/word"
+//)
+//
+//type Graph struct {
+//	bits    *bit.bits
+//	actions *action.actions
+//}
 
-type Graph struct {
-	bits    *bit.bits
-	actions *action.actions
-}
-
-func NewGraph(storage internal.storage) *Graph {
-	bits := bit.newBits(storage)
-	codes := code.newCodes(storage, bits)
-	characters := word.newCharacters(storage, codes)
-	words := word.newWords(storage, characters)
-	actions := action.newActions(storage, words)
-
-	return &Graph{
-		bits:    bits,
-		actions: actions,
-	}
-}
-
-func (g *Graph) InitializeBits() error {
-	return g.bits.initialize()
-}
-
-func (g *Graph) AddAction(name string) (action.Action, error) {
-	return g.actions.create(name)
-}
+//func NewGraph(storage internal.storage) *Graph {
+//	bits := bit.newBits(storage)
+//	codes := code.newCodes(storage, bits)
+//	characters := word.newCharacters(storage, codes)
+//	words := word.newWords(storage, characters)
+//	actions := action.newActions(storage, words)
+//
+//	return &Graph{
+//		bits:    bits,
+//		actions: actions,
+//	}
+//}
+//
+//func (g *Graph) InitializeBits() error {
+//	return g.bits.initialize()
+//}
+//
+//func (g *Graph) AddAction(name string) (action.Action, error) {
+//	return g.actions.create(name)
+//}
--- internal/remark/repository.go
+++ internal/remark/repository.go
@@ -1,1 +1,33 @@
 package remark
+
+import "github.com/mem-memov/semnet/internal/detail"
+
+type Repository struct {
+	detailRepository detail.Repository
+}
+
+func NewRepository(detailRepository detail.Repository) *Repository {
+	return &Repository{
+		detailRepository: detailRepository,
+	}
+}
+
+func (r *Repository) StartStory(object string, property string) (Entity, error) {
+
+	detailEntity, err := r.detailRepository.Provide(object, property)
+	if err != nil {
+		return Entity{}, err
+	}
+
+	return Entity{}, nil
+}
+
+func (r *Repository) AddRemark(previousIdentifier uint, object string, property string) (Entity, error) {
+
+	detailEntity, err := r.detailRepository.Provide(object, property)
+	if err != nil {
+		return Entity{}, err
+	}
+
+	return Entity{}, nil
+}
